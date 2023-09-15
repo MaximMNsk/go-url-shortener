9732b664069642a686ebe2a7be11f471c75dfed6
Maxim M <maxim.m.nsk@gmail.com>
2023-09-15 12:10:24 +0700

    Iter2. Ver 0.4.6 test body.Close fix

--- cmd/shortener/main_test.go
+++ cmd/shortener/main_test.go
@@ -81,7 +81,7 @@ func Test_handleMainPage(t *testing.T) {
 				result := w.Result()
 				assert.Equal(t, tt.want.statusCode, result.StatusCode)
 				assert.Contains(t, result.Header.Get("Content-Type"), tt.want.contentType)
-				return
+				_ = result.Body.Close()
 			}
 
 			if tt.name == "Set link" {
@@ -100,7 +100,7 @@ func Test_handleMainPage(t *testing.T) {
 				shortLink = string(linkResult)
 				fmt.Println(shortLink)
 				require.NotEmpty(t, shortLink)
-				return
+				_ = result.Body.Close()
 			}
 
 			if tt.name == "Get link" {
@@ -112,7 +112,7 @@ func Test_handleMainPage(t *testing.T) {
 				assert.Equal(t, tt.want.statusCode, result.StatusCode)
 				assert.Contains(t, result.Header.Get("Content-Type"), tt.want.contentType)
 				assert.Equal(t, result.Header.Get("Location"), tt.want.response)
-				return
+				_ = result.Body.Close()
 			}
 
 		})
