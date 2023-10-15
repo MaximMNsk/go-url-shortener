17db86c3e8c56159986c613a04dbac374f5a8272
Maks <maxim.m.nsk@gmail.com>
2023-10-15 17:02:51 +0700

    Iter10. Fix 3

--- internal/storage/db/db.go
+++ internal/storage/db/db.go
@@ -9,23 +9,24 @@ import (
 )
 
 var (
-	db *pgx.Conn
+	DB *pgx.Conn
 )
 
 func Connect() error {
 	ctx := context.Background()
+	logger.PrintLog(logger.INFO, config.Config.Final.DB)
 	database, err := pgx.Connect(ctx, config.Config.Final.DB)
-	db = database
+	DB = database
 	return err
 }
 
 func GetDB() *pgx.Conn {
-	return db
+	return DB
 }
 
 func Close() {
 	ctx := context.Background()
-	err := db.Close(ctx)
+	err := DB.Close(ctx)
 	if err != nil {
 		logger.PrintLog(logger.ERROR, "Can't close connection")
 	}
