558a1616103481cb7ea08c973b39a5037e781e04
Maks <maxim.m.nsk@gmail.com>
2023-10-15 16:40:28 +0700

    Iter10. Fix

---
+++ internal/storage/db/db.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"context"
+	"github.com/MaximMNsk/go-url-shortener/internal/util/logger"
+	"github.com/MaximMNsk/go-url-shortener/server/config"
+	"github.com/jackc/pgx/v5"
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+var (
+	db *pgx.Conn
+)
+
+func Connect() error {
+	ctx := context.Background()
+	database, err := pgx.Connect(ctx, config.Config.Final.Db)
+	db = database
+	return err
+}
+
+func GetDB() *pgx.Conn {
+	return db
+}
+
+func Close() {
+	ctx := context.Background()
+	err := db.Close(ctx)
+	if err != nil {
+		logger.PrintLog(logger.ERROR, "Can't close connection")
+	}
+}
