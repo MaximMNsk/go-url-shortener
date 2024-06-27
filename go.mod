fad3e572da2a16fc168ec7920eef051f5dc89c24
MaksimMedyantsev <maxim.m.nsk@gmail.com>
2024-06-27 11:40:44 +0700

    Vet autotests 1

--- go.mod
+++ go.mod
@@ -12,7 +12,6 @@ require (
 	github.com/jackc/pgerrcode v0.0.0-20220416144525-469b46aa5efa
 	github.com/jackc/pgx/v5 v5.6.0
 	github.com/kisielk/errcheck v1.7.0
-	github.com/pashagolub/pgxmock/v4 v4.1.0
 	github.com/rs/zerolog v1.30.0
 	github.com/stretchr/testify v1.9.0
 	golang.org/x/tools v0.17.0
--- server/server/handler.go
+++ server/server/handler.go
@@ -25,7 +25,6 @@ import (
 	"github.com/jackc/pgx/v5/pgconn"
 	"io"
 	"net/http"
-	"net/http/pprof"
 )
 
 // ErrorHandlers - тип для работы с ошибками слоя обработчиков.
@@ -382,6 +381,11 @@ func (s *Server) HandlePing(res http.ResponseWriter, req *http.Request) {
 // HandleOther - middleware для обработки неожидаемых запросов.
 func HandleOther(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		logger.PrintLog(logger.DEBUG, `Method: `+req.Method, true)
+		//body, _ := io.ReadAll(req.Body)
+		//defer req.Body.Close()
+		//logger.PrintLog(logger.DEBUG, `Body: `+string(body), true)
+		//logger.PrintLog(logger.DEBUG, req.Method, true)
 		if req.Method == http.MethodGet || req.Method == http.MethodPost || req.Method == http.MethodDelete {
 			next.ServeHTTP(res, req)
 			return
@@ -497,17 +501,17 @@ func (s *Server) Start(ctx context.Context) error {
 		s.Routers.With(extlogger.Log)
 	}
 	s.Routers.Route("/", func(r chi.Router) {
-		s.Routers.Group(func(r chi.Router) {
-			r.HandleFunc("/debug/pprof/*", pprof.Index)
-			r.HandleFunc("/debug/pprof/profile", pprof.Profile)
-			r.Handle("/debug/pprof/heap", pprof.Handler("heap"))
-		})
+		//s.Routers.Group(func(r chi.Router) {
+		//	r.HandleFunc("/debug/pprof/*", pprof.Index)
+		//	r.HandleFunc("/debug/pprof/profile", pprof.Profile)
+		//	r.Handle("/debug/pprof/heap", pprof.Handler("heap"))
+		//})
 		s.Routers.Group(func(r chi.Router) {
 			r.Use(cookie.AuthSetter)
 
 			r.Post(`/`, s.HandlePOST)
-			r.Post(`/api/{query}`, s.HandleAPIBatch)
-			r.Post(`/api/shorten/{query}`, s.HandleAPIShorten)
+			r.Post(`/api/{query}`, s.HandleAPIShorten)
+			r.Post(`/api/shorten/{query}`, s.HandleAPIBatch)
 			r.Get(`/ping`, s.HandlePing)
 			r.Get(`/{query}`, s.HandleGET)
 		})
